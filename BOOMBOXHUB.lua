07fc67a8af20d8d7e572dd05fdec32504f7329af
Redcarred2 <57776274+Redcarred2@users.noreply.github.com>
2021-02-03 16:49:49 -0700

    lel

--- BOOMBOXHUB.lua
+++ BOOMBOXHUB.lua
@@ -7,46 +7,6 @@ local Steal = Instance.new("TextButton")
 local Steal_Roundify_12px = Instance.new("ImageLabel")
 local Visualizer = Instance.new("TextButton")
 local Visualizer_Roundify_12px = Instance.new("ImageLabel")
-HumanDied = false
-local CountSCIFIMOVIELOL = 1
-function SCIFIMOVIELOL(Part0,Part1,Position,Angle)
-	local AlignPos = Instance.new('AlignPosition', Part1); AlignPos.Name = "AliP"..CountSCIFIMOVIELOL
-	AlignPos.ApplyAtCenterOfMass = true;
-	AlignPos.MaxForce = 5772000
-	AlignPos.MaxVelocity = math.huge/9e110;
-	AlignPos.ReactionForceEnabled = false;
-	AlignPos.Responsiveness = 200;
-	AlignPos.RigidityEnabled = false;
-	local AlignOri = Instance.new('AlignOrientation', Part1); AlignOri.Name = "AliO"..CountSCIFIMOVIELOL
-	AlignOri.MaxAngularVelocity = math.huge/9e110;
-	AlignOri.MaxTorque = 5772000
-	AlignOri.PrimaryAxisOnly = false;
-	AlignOri.ReactionTorqueEnabled = false;
-	AlignOri.Responsiveness = 200;
-	AlignOri.RigidityEnabled = false;
-	local AttachmentA=Instance.new('Attachment',Part1); AttachmentA.Name = "Ath"..CountSCIFIMOVIELOL
-	local AttachmentB=Instance.new('Attachment',Part0); AttachmentB.Name = "Ath"..CountSCIFIMOVIELOL
-	AttachmentA.Orientation = Angle or Vector3.new(0,0,0)
-	AttachmentA.Position = Position or Vector3.new(0,0,0)
-	AlignPos.Attachment1 = AttachmentA;
-	AlignPos.Attachment0 = AttachmentB;
-	AlignOri.Attachment1 = AttachmentA;
-	AlignOri.Attachment0 = AttachmentB;
-	CountSCIFIMOVIELOL = CountSCIFIMOVIELOL + 1
-	return {AlignPos,AlignOri,AttachmentA,AttachmentB}
-end
-
-if _G.netted ~= true then
-	_G.netted = true
-	coroutine.wrap(function()
-		settings().Physics.PhysicsEnvironmentalThrottle = Enum.EnviromentalPhysicsThrottle.Disabled
-		settings().Physics.AllowSleep = false
-		game:GetService("RunService").RenderStepped:Connect(function()
-			game:FindFirstChildOfClass("Players").LocalPlayer.MaximumSimulationRadius=math.pow(math.huge,math.huge)
-			sethiddenproperty(game:FindFirstChildOfClass("Players").LocalPlayer,"SimulationRadius",math.huge*math.huge)
-		end)
-	end)()
-end
 
 Boombox.Name = "Boombox"
 Boombox.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
